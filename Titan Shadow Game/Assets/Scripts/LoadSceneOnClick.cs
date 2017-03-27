1791832cf517f0bf7996912eccefb59a5cdb4f56
daviematz <daviematz@gmail.com>
2017-03-26 23:07:30 -0400

    First commiit...
    For some reason this is 882 changes? Maybe just because it is my first
    commit I will have to look into it.

---
+++ Titan Shadow Game/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSceneOnClick : MonoBehaviour {
+
+	public void LoadByIndex(int index)
+    {
+        if(index == -1)
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+        else
+            Application.LoadLevel(index);
+    }
+}
